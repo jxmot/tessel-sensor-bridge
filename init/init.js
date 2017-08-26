fb7374cdcb7de12334aeb94de57befdeecbb4fa2
jxmot <webdev@stupiddog.net>
2017-08-26 11:40:12 -0500

    sensor config data creation utility, accessible via server for client

---
+++ init/init.js
@@ -0,0 +1,36 @@
+'use strict'
+/*
+    (c) 2017 J.Motyl
+
+    Initialize any static data that the application requires.
+*/
+const path = require('path');
+const mLab = require(path.join(global.apphome, '/mlab-restapi/mlab-restapi.js'));
+
+const initdata = require(path.join(global.apphome, '/init/initdata.json'));
+var _initdata = [];
+
+// For triggering and handing events
+const events = require('events');
+const eventEmitter = new events.EventEmitter();
+eventEmitter.once('initDone', initDone);
+
+var init = {};
+
+// config->sensorconfigs
+init.init = function(cb) {
+    initdata.sensorconfigs.forEach(function(sensor, index, array) {
+        mLab.insertDoc(sensor, function(doc) {
+            console.log(doc);
+            _initdata.push(JSON.parse(doc));
+            if(index === (array.length - 1)) eventEmitter.emit('initDone', cb);
+        },
+        'config');
+    });
+};
+
+function initDone(cb) {
+    cb(JSON.stringify(_initdata, null, 2));
+};
+
+module.exports = init;
