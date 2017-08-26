df41e08a474105d8a20b6705f4501a87a841337f
jxmot <webdev@stupiddog.net>
2017-08-26 00:41:35 -0500

    application source

---
+++ server.js
@@ -0,0 +1,50 @@
+'use strict'
+/*
+    (c) 2017 J.Motyl
+
+    tessel-sensor-bridge - Server
+
+    Provides and API for locally networked IoT devices. Initially
+    the devices were ESP8266-12E boards with DHT22 sensors. 
+
+*/
+const http = require('http');
+const server = http.createServer().listen(4843);
+
+const qstring = require('querystring');
+
+// https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html
+// https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+
+server.on('request', function (req, res) {
+
+    console.log(req.method + '   ' + req.url);
+
+    switch(req.method)
+    {
+        case 'POST':
+            var body = '';
+            req.on('data', function (data){
+                body += data;
+            });
+            req.on('end', function () {
+
+                console.log(body);
+
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end();
+            });
+            break;
+
+        case 'GET':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end();
+            break;
+
+        default:
+            // Method Not Allowed
+            res.writeHead(405, {'Content-Type': 'text/plain','Allow': 'GET,POST'});
+            res.end();
+            break;
+    };
+});
