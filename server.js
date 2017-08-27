81c0c17c59af5b0cd27966e9b30ca12ecb3143d9
jxmot <webdev@stupiddog.net>
2017-08-27 04:19:54 -0500

    comments and cleanup

--- server.js
+++ server.js
@@ -11,34 +11,39 @@
 global.apphome = __dirname;
 
 const path = require('path');
-const http = require('http');
 
+// prepare the routes
 const routes = require(path.join(global.apphome, '/routes'));
-
+// set up the sensor API server
+const http = require('http');
 const apiServer = http.createServer().listen(4843);
 
+//////////////////////////////////////////////////////////////////////////////
+// HTTPS Server - Provides an html application for viewing the sensors.
+// 
 // https://docs.nodejitsu.com/articles/HTTP/servers/how-to-create-a-HTTPS-server/
 const fs = require('fs');
 const https = require('https');
-
+// use SSL certificate and key files created with openSSL
 const options = {
     key: fs.readFileSync(path.join(global.apphome, '/cert/key.pem')),
     cert: fs.readFileSync(path.join(global.apphome, '/cert/cert.pem'))
 };
-
 const htmServer = https.createServer(options).listen(443);
+//
+//////////////////////////////////////////////////////////////////////////////
 
 // https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html
 // https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
 
 apiServer.on('request', function (req, res) {
-    console.log(req.method + '   ' + req.url);
-    routes.router(req, res);
+    console.log('apiServer : ' + req.method + '   ' + req.url);
+    routes.apiRouter(req, res);
 });
 
 htmServer.on('request', function (req, res) {
-    console.log(req.method + '   ' + req.url);
-    routes.router(req, res);
+    console.log('htmServer : ' + req.method + '   ' + req.url);
+    routes.htmRouter(req, res);
 });
 
 
