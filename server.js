87d52b89c47db9cb65a5c5840f895079a10ea2e1
jxmot <webdev@stupiddog.net>
2017-08-27 00:09:19 -0500

    see decsription
    added 2nd server, https. moved routing into /routes.

--- server.js
+++ server.js
@@ -12,52 +12,33 @@ global.apphome = __dirname;
 
 const path = require('path');
 const http = require('http');
-const server = http.createServer().listen(4843);
 
-//const qstring = require('querystring');
+const routes = require(path.join(global.apphome, '/routes'));
+
+const apiServer = http.createServer().listen(4843);
+
+// https://docs.nodejitsu.com/articles/HTTP/servers/how-to-create-a-HTTPS-server/
+const fs = require('fs');
+const https = require('https');
+
+const options = {
+    key: fs.readFileSync(path.join(global.apphome, '/cert/key.pem')),
+    cert: fs.readFileSync(path.join(global.apphome, '/cert/cert.pem'))
+};
+
+const htmServer = https.createServer(options).listen(443);
 
 // https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html
 // https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
 
-server.on('request', function (req, res) {
-
+apiServer.on('request', function (req, res) {
     console.log(req.method + '   ' + req.url);
-
-    switch(req.method)
-    {
-        case 'POST':
-            var body = '';
-            req.on('data', function (data){
-                body += data;
-            });
-            req.on('end', function () {
-
-                console.log(body);
-
-                res.writeHead(200, {'Content-Type': 'text/plain'});
-                res.end();
-            });
-            break;
-
-        case 'GET':
-            if(req.url.includes('init')) {
-                const init = require(path.join(global.apphome, '/init/init.js'));
-                init.init(function(data) {
-                    res.writeHead(200, {'Content-Type': 'text/plain'});
-                    res.write(data);
-                    res.end();
-                });
-            } else {
-                res.writeHead(200, {'Content-Type': 'text/plain'});
-                res.end();
-            }
-            break;
-
-        default:
-            // Method Not Allowed
-            res.writeHead(405, {'Content-Type': 'text/plain','Allow': 'GET,POST'});
-            res.end();
-            break;
-    };
+    routes.router(req, res);
 });
+
+htmServer.on('request', function (req, res) {
+    console.log(req.method + '   ' + req.url);
+    routes.router(req, res);
+});
+
 
