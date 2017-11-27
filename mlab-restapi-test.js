73d2211a36607a31e7dcdd4b142f98bc73beecb9
jxmot <webdev@stupiddog.net>
2017-11-27 15:31:59 -0600

    improved debug output

--- mlab-restapi-test.js
+++ mlab-restapi-test.js
@@ -16,7 +16,7 @@ const mLab = require(path.join(global.apphome, '/mlab-restapi/mlab-restapi.js'))
 // /mlab-restapi/config/_mlab-account.js, see 
 // /mlab-restapi/config/example_mlab-account.js for more information)
 mLab.getCollections(function(data) {
-    console.log(data);
+    console.log('getCollections - ' + data);
 });
 
 // get all documents in a collection. you can pass an optional
@@ -26,31 +26,31 @@ mLab.getCollections(function(data) {
 // /mlab-restapi/config/_mlab-account.js (see 
 // /mlab-restapi/config/example_mlab-account.js for more information)
 mLab.getAllDocs(function(data) {
-    console.log(data);
+    console.log('getAllDocs - ' + data);
 });
 
 // query for a specific document
 mLab.queryDocs({_id:'SENS02'}, function(data) {
-    console.log(data);
+    console.log('queryDocs 1 - ' + data);
 });
 
 mLab.queryDocs({_id:'SENS01'}, function(data) {
-    console.log(data);
+    console.log('queryDocs 2 - ' + data);
 });
 
 // insert a document
 mLab.insertDoc({_id:'SENS31',t:'33.2F',h:'59%'}, function(data) {
-    console.log(data);
+    console.log('insertDoc - ' + data);
     mLab.queryDocs({_id:'SENS31'},function(data) {
-        console.log(data);
+        console.log('queryDocs 3 - ' + data);
         var _data = JSON.parse(data);
         for(var ix = 0;ix < _data.length;ix++) {
             _data[ix].t = '39.6F';
         }
         mLab.updateDoc({_id:'SENS31'}, _data, function(data) {
-            console.log(data);
+            console.log('updateDoc - ' + data);
             mLab.deleteDoc('SENS31', function(data) {
-                console.log(data);
+                console.log('deleteDoc - ' + data);
             });
         });
     });
