967fb00998302e784ea39b9d6096a2f17f4a074d
jxmot <webdev@stupiddog.net>
2017-11-27 23:46:44 -0600

    added server-cfg

---
+++ config/server-cfg.js
@@ -0,0 +1,18 @@
+'use strict'
+/*
+    (c) 2017 J.Motyl
+
+    Application Server Configuration(s)
+*/
+module.exports = {
+    http: {
+        port: 80
+    },
+    devapi: {
+        port: 3000
+    },
+    udp: {
+        port: 31234
+    }
+};
+
--- server.js
+++ server.js
@@ -15,18 +15,24 @@ global.apphome = __dirname;
 
 const path = require('path');
 
+// config params for our servers
+const srvcfg = require(path.join(global.apphome, '/config/server-cfg.js'));
+
 // prepare the routes
 const routes = require(path.join(global.apphome, '/routes'));
+
 //////////////////////////////////////////////////////////////////////////////
 // HTTP API Server - Provides an API for sensor devices
-const apiServer = require('http').createServer().listen(4843);
+const apiServer = require('http').createServer().listen(srvcfg.devapi.port);
+console.log('apiServer listening : ' + srvcfg.devapi.port);
 apiServer.on('request', function (req, res) {
     console.log('apiServer : ' + req.method + '   ' + req.url);
     routes.apiRouter(req, res);
 });
 //////////////////////////////////////////////////////////////////////////////
 // HTTP Application Server - Provides an html app for viewing the sensors.
-const htmServer = require('http').createServer().listen(80);
+const htmServer = require('http').createServer().listen(srvcfg.http.port);
+console.log('htmServer listening : ' + srvcfg.http.port);
 htmServer.on('request', function (req, res) {
     console.log('htmServer : ' + req.method + '   ' + req.url);
     routes.htmRouter(req, res);
